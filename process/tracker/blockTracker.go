deb0188e69d2e85fdb78e59e8d25707a3ad00a83
Marius C <marius.ciucam@gmail.com>
2025-05-14 19:09:38 +0300

    FEAT: Redesign solution for simpler chain tracking

--- process/tracker/blockTracker.go
+++ process/tracker/blockTracker.go
@@ -3,6 +3,7 @@ package tracker
 import (
 	"context"
 	"fmt"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
@@ -13,18 +14,23 @@ import (
 
 type blockTracker struct {
 	client           ETHClientHandler
-	eventsByBlock    map[uint64][]types.Log // todo: here cacher component + cacher for blocks
+	blocks           map[common.Hash]*types.Header // todo: here cacher component + cacher for blocks
 	minConfirmations uint8
 
 	addresses []common.Address
 	topics    [][]common.Hash
+
+	finalizedBlockNonce uint64
+
+	incomingHeaderCreator IncomingHeaderCreator
 }
 
 // todo: here, pass directly expected eth data, not our config
 
-func NewBlockTracker(subscribedEvents []config.SubscribedEvent) *blockTracker {
+func NewBlockTracker(subscribedEvents []config.SubscribedEvent, client ETHClientHandler) *blockTracker {
+
 	return &blockTracker{
-		eventsByBlock:    make(map[uint64][]types.Log),
+		blocks:           map[common.Hash]*types.Header{},
 		minConfirmations: 2,
 	}
 }
@@ -32,8 +38,14 @@ func NewBlockTracker(subscribedEvents []config.SubscribedEvent) *blockTracker {
 func (bt *blockTracker) Start(ctx context.Context, errChan chan error) {
 	// also somewhere get latest block on a separate go routine to check block finality perhaps
 
+	latestHeader, err := bt.client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		return
+	}
+
+	bt.finalizedBlockNonce = latestHeader.Nonce.Uint64() - uint64(bt.minConfirmations)
+
 	go bt.subscribeToNewHeaders(ctx, errChan)
-	go bt.subscribeToEvents(ctx, errChan)
 }
 
 func (bt *blockTracker) subscribeToNewHeaders(ctx context.Context, errChan chan error) {
@@ -51,44 +63,32 @@ func (bt *blockTracker) subscribeToNewHeaders(ctx context.Context, errChan chan 
 			errChan <- fmt.Errorf("header subscription error: %v", err)
 			return
 		case header := <-headers:
-			bt.processBlock(header)
+			bt.processBlock(ctx, header, errChan)
 		case <-ctx.Done():
 			return
 		}
 	}
 }
 
-func (bt *blockTracker) processBlock(header *types.Header) {
-	_ = header
-}
-
-func (bt *blockTracker) subscribeToEvents(ctx context.Context, errChan chan error) {
-	logs := make(chan types.Log)
+func (bt *blockTracker) processBlock(ctx context.Context, header *types.Header, errChan chan error) {
 	query := ethereum.FilterQuery{
+		FromBlock: header.Number,
+		ToBlock:   header.Number,
 		Addresses: bt.addresses,
 		Topics:    bt.topics,
 	}
-	sub, err := bt.client.SubscribeFilterLogs(ctx, query, logs)
+
+	logs, err := bt.client.FilterLogs(ctx, query)
 	if err != nil {
 		errChan <- fmt.Errorf("failed to subscribe to events: %v", err)
 		return
 	}
 
-	defer sub.Unsubscribe()
-
-	for {
-		select {
-		case err := <-sub.Err():
-			errChan <- fmt.Errorf("event subscription error: %v", err)
-			return
-		case log := <-logs:
-			bt.processEventLog(log)
-		case <-ctx.Done():
-			return
-		}
+	finalizedHeader, err := bt.client.HeaderByNumber(ctx, big.NewInt(int64(bt.finalizedBlockNonce)))
+	if err != nil {
+		errChan <- fmt.Errorf("failed to subscribe to events: %v", err)
+		return
 	}
-}
 
-func (bt *blockTracker) processEventLog(log types.Log) {
-	_ = log
+	bt.incomingHeaderCreator.CreateIncomingHeader(finalizedHeader, logs)
 }
--- process/tracker/interface.go
+++ process/tracker/interface.go
@@ -2,13 +2,21 @@ package tracker
 
 import (
 	"context"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/multiversx/mx-chain-core-sovereign-go/data/sovereign"
 )
 
 type ETHClientHandler interface {
 	SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription, error)
+	FilterLogs(ctx context.Context, q ethereum.FilterQuery) ([]types.Log, error)
 	SubscribeFilterLogs(ctx context.Context, q ethereum.FilterQuery, ch chan<- types.Log) (ethereum.Subscription, error)
+	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
 	Close()
+}
+
+type IncomingHeaderCreator interface {
+	CreateIncomingHeader(header *types.Header, logs []types.Log) (sovereign.IncomingHeaderHandler, error)
 }
